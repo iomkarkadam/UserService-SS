fd87e7f96bd26e98f7de378df5f1c2b692316948
iomkarkadam <omkarsk.0101@gmail.com>
2024-11-17 16:55:35 +0530

    initial commit- added simple springboot application

---
+++ UserServiceForScalableServices/UserServiceForScalableServices/src/main/java/com/bits/UserServiceForScalableServicesApplication.java
@@ -0,0 +1,13 @@
+package com.bits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceForScalableServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceForScalableServicesApplication.class, args);
+	}
+
+}
